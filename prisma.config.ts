1f26f7c0dbd1edf1c75e7b4e795b601d5b6980f5
ruchi551 <ruchukumari989@gmail.com>
2026-04-25 18:48:11 +0530

    add neon database config

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,13 +5,13 @@ export default defineConfig({
   earlyAccess: true,
   schema: path.join("prisma", "schema.prisma"),
   datasource: {
-    url: "postgresql://postgres:postgres123@localhost:5432/expense_tracker",
+    url: "postgresql://neondb_owner:npg_lxs4fZQhzKy5@ep-old-sky-amg3myeo-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
   migrate: {
     async adapter() {
       const { PrismaPg } = await import("@prisma/adapter-pg");
       return new PrismaPg({
-        connectionString: "postgresql://postgres:postgres123@localhost:5432/expense_tracker",
+        connectionString: "postgresql://neondb_owner:npg_lxs4fZQhzKy5@ep-old-sky-amg3myeo-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
       });
     },
   },
